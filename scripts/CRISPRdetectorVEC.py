80921e890b74295c930f0951e0ac504f7a5d24ef
hlcas <93194094+hlcas@users.noreply.github.com>
2022-02-08 14:20:58 +0800

    Update CRISPRdetectorVEC.py

--- scripts/CRISPRdetectorVEC.py
+++ scripts/CRISPRdetectorVEC.py
@@ -114,6 +114,3 @@ else:
 logger.info('Finished : variants called')
 time1=time.time()
 logger.info('Finished! Running time: %s seconds'%(round(time1-time0,2)))
-
-if __name__ == '__main__':
-	main()
