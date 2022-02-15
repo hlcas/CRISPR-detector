e6f85b451ce179a116bba720ffe2da773c1fff35
hlcas <93194094+hlcas@users.noreply.github.com>
2022-02-15 09:32:05 +0800

    Update work.sh

--- SPACE/work.sh
+++ SPACE/work.sh
@@ -1,1 +1,1 @@
-
+python ../scripts/CRISPRdetectorBE.py --e1 ABE_S14_SPACE.1.fq.gz --e2 AB _S14_SPACE.2.fq.gz --c1 ABE_S14_control.1.fq.gz --c2 ABE_S14_control.2.fq.gz --sample ABE_S14 --amplicons_file ABE_S14.tab
