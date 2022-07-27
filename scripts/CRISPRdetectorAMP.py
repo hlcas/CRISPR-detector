0be74f0dd15717720730749fc06179bfb32ee37f
hlcas <93194094+hlcas@users.noreply.github.com>
2022-07-27 12:09:35 +0800

    Update CRISPRdetectorAMP.py

--- scripts/CRISPRdetectorAMP.py
+++ scripts/CRISPRdetectorAMP.py
@@ -46,6 +46,7 @@ python CRISPRdetectorAMP.py
 --window_size: defines the size (in bp) of the quantification window extending from the position specified by the cleavage_offset parameter in relation to the provided guide RNA sequence, 0 means whole amplicon analysis [default:0]
 --ignore_substitutions: enable substitutions evaluation [default:0]  
 --min_num_of_reads: the minimum number of reads (per locus site) to evaluate [default:500] 
+
 ------------------------------------------------------------------------------------------------------------------------
 '''
 parse = argparse.ArgumentParser(prog='PROG', formatter_class=argparse.RawDescriptionHelpFormatter, description=textwrap.dedent(description))
