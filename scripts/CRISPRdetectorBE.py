0df96840b8179a47beacb100edf7cfa095b95f11
hlcas <93194094+hlcas@users.noreply.github.com>
2022-07-27 12:08:02 +0800

    Update CRISPRdetectorBE.py

--- scripts/CRISPRdetectorBE.py
+++ scripts/CRISPRdetectorBE.py
@@ -36,6 +36,14 @@ python CRISPRdetectorBE.py
 --threads: number of threads to run sentieon minimap2 & driver module [default:1] 
 --min_allele_frac: the minimum allelic fraction in treated sample [default:0.005] 
 --max_fisher_pv_active: the maximum pvalue of the statistical difference between treated and untreated sample [default:0.05] 
+--amplicons_file: a tab-delimited text amplicons description file with up to 3 columns: AMPLICON_NAME, AMPLICON_SEQ, gRNA_SEQ_without_PAM(optional) [required]  
+--anno: annotate variants with ANNOVAR or not [optional]
+--assembly: assembly version, hg19,hg38 ... [optional]
+--db: ANNOVAR database path [optional]
+--ClinVar: only organism homo sapiens experiment type sequencing data support variant annotations from ClinVar [default:0]  
+--cleavage_offset: center of quantification window to use within respect to the 3-end of the provided sgRNA sequence [default:-3]
+--window_size: defines the size (in bp) of the quantification window extending from the position specified by the cleavage_offset parameter in relation to the provided guide RNA sequence, 0 means whole amplicon analysis [default:0]
+--min_num_of_reads: the minimum number of reads (per locus site) to evaluate [default:500] 
 ------------------------------------------------------------------------------------------------------------------------
 '''
 parse = argparse.ArgumentParser(prog='PROG', formatter_class=argparse.RawDescriptionHelpFormatter, description=textwrap.dedent(description))
