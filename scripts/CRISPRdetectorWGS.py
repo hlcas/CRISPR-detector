5f15ee2e5d9b5844825d08ca148f19e980bf1a51
hlcas <93194094+hlcas@users.noreply.github.com>
2022-08-23 17:25:47 +0800

    Update CRISPRdetectorWGS.py

--- scripts/CRISPRdetectorWGS.py
+++ scripts/CRISPRdetectorWGS.py
@@ -14,6 +14,7 @@ import time
 import logging
 import argparse
 import textwrap
+import pandas as pd
 
 description = '''
 ------------------------------------------------------------------------------------------------------------------------
